cc03d77edd388e4779160535ec6cbae32c0fb5f0
Jimmy Sandoval <jsandovaldev.pe@gmail.com>
2024-01-30 22:45:30 -0500

    feat: Add responsive design to how works section

--- src/components/HowWorks.vue
+++ src/components/HowWorks.vue
@@ -12,8 +12,10 @@
       </p>
     </div>
 
-    <div class="w-full flex items-start justify-center gap-[6.25rem]">
-      <div class="w-full max-w-[38.75rem]">
+    <div
+      class="w-full flex flex-col-reverse lg:flex-row items-center lg:items-start justify-center gap-[6.25rem]"
+    >
+      <div class="w-full max-w-[400px] lg:max-w-[38.75rem]">
         <img
           class="w-full object-cover"
           src="../assets/images/howworks.webp"
@@ -21,48 +23,66 @@
         />
       </div>
 
-      <div class="flex flex-col items-start gap-[2rem]">
-        <h3 class="text-[1.75rem] text-primary">Pasos para lograr tus metas</h3>
+      <div class="w-full max-w-[30rem] flex flex-col items-center lg:items-start gap-[2rem]">
+        <h3 class="hidden lg:inline-flex text-[1.75rem] text-primary">
+          Pasos para lograr tus metas
+        </h3>
         <div class="relative flex flex-col gap-[3.4375rem]">
-          <div class="h-full w-[0.0813rem] bg-primary absolute top-0 left-[2.3125rem] -z-[1]"></div>
+          <div
+            class="hidden lg:block h-full w-[0.0813rem] bg-primary absolute top-0 left-[2.3125rem] -z-[1]"
+          ></div>
 
-          <div class="group grid grid-cols-[4.75rem_1fr] items-center gap-[2.5rem]">
+          <div
+            class="group grid grid-cols-1 lg:grid-cols-[4.75rem_1fr] gap-[1.5rem] justify-items-center lg:items-center lg:gap-[2.5rem]"
+          >
             <div
-              class="w-[4.75rem] h-[4.75rem] bg-white rounded-full flex items-center border-[1.5px] border-primary justify-center text-primary text-[2.5rem] group-hover:bg-primary group-hover:text-white transition-colors select-none"
+              class="w-[4.75rem] h-[4.75rem] bg-white rounded-full flex items-center border-[1.5px] border-primary justify-center text-primary text-[2.5rem] lg:group-hover:bg-primary lg:group-hover:text-white transition-colors select-none"
             >
               1
             </div>
             <div class="flex flex-col gap-[0.625rem]">
-              <h6 class="text-[1.25rem] text-primary">Regístrate</h6>
-              <p class="font-raleway font-normal text-[0.875rem] text-secondary leading-[1.5rem]">
+              <h6 class="text-[1.25rem] text-primary text-center lg:text-left">Regístrate</h6>
+              <p
+                class="font-raleway font-normal text-[0.875rem] text-secondary leading-[1.5rem] text-center lg:text-left"
+              >
                 Únete con nosotros y logra tus objetivos a corto, mediano y largo plazo, accediendo
                 a los mejores cursos
               </p>
             </div>
           </div>
-          <div class="group grid grid-cols-[4.75rem_1fr] items-center gap-[2.5rem]">
+          <div
+            class="group grid grid-cols-1 lg:grid-cols-[4.75rem_1fr] gap-[1.5rem] justify-items-center lg:items-center lg:gap-[2.5rem]"
+          >
             <div
-              class="w-[4.75rem] h-[4.75rem] bg-white rounded-full flex items-center border-[0.0938rem] border-primary justify-center text-primary text-[2.5rem] group-hover:bg-primary group-hover:text-white transition-colors select-none"
+              class="w-[4.75rem] h-[4.75rem] bg-white rounded-full flex items-center border-[0.0938rem] border-primary justify-center text-primary text-[2.5rem] lg:group-hover:bg-primary lg:group-hover:text-white transition-colors select-none"
             >
               2
             </div>
             <div class="flex flex-col gap-[0.625rem]">
-              <h6 class="text-[1.25rem] text-primary">Pagos seguros</h6>
-              <p class="font-raleway font-normal text-[0.875rem] text-secondary leading-[1.5rem]">
+              <h6 class="text-[1.25rem] text-primary text-center lg:text-left">Pagos seguros</h6>
+              <p
+                class="font-raleway font-normal text-[0.875rem] text-secondary leading-[1.5rem] text-center lg:text-left"
+              >
                 Aceptamos todo tipo de tarjeta, ofrecemos seguridad y confiabilidad en tus pagos.
                 Utilizando tecnologías de alta calidad.
               </p>
             </div>
           </div>
-          <div class="group grid grid-cols-[4.75rem_1fr] items-center gap-[2.5rem]">
+          <div
+            class="group grid grid-cols-1 lg:grid-cols-[4.75rem_1fr] gap-[1.5rem] justify-items-center lg:items-center lg:gap-[2.5rem]"
+          >
             <div
-              class="w-[4.75rem] h-[4.75rem] bg-white rounded-full flex items-center border-[0.0938rem] border-primary justify-center text-primary text-[2.5rem] group-hover:bg-primary group-hover:text-white transition-colors select-none"
+              class="w-[4.75rem] h-[4.75rem] bg-white rounded-full flex items-center border-[0.0938rem] border-primary justify-center text-primary text-[2.5rem] lg:group-hover:bg-primary lg:group-hover:text-white transition-colors select-none"
             >
               3
             </div>
             <div class="flex flex-col gap-[0.625rem]">
-              <h6 class="text-[1.25rem] text-primary">Certifícate con nosotros</h6>
-              <p class="font-raleway font-normal text-[0.875rem] text-secondary leading-[1.5rem]">
+              <h6 class="text-[1.25rem] text-primary text-center lg:text-left">
+                Certifícate con nosotros
+              </h6>
+              <p
+                class="font-raleway font-normal text-[0.875rem] text-secondary leading-[1.5rem] text-center lg:text-left"
+              >
                 Certifícate y transforma tu camino financiero. Abre puertas a nuevas oportunidades
                 en el trading y forex
               </p>
