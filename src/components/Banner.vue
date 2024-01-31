2326088f479adddeafd571b5798fc9ec11b798d2
Jimmy Sandoval <jsandovaldev.pe@gmail.com>
2024-01-30 22:53:04 -0500

    feat: Update banner image

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,16 +1,9 @@
 <template>
-  <section class="w-full relative h-[25rem] inline-flex">
+  <section class="w-full relative max-h-[25rem] inline-flex">
     <img
       class="w-full h-full object-cover object-center"
       src="../assets/images/banner.webp"
       alt="Banner"
     />
-
-    <h1
-      class="w-full max-w-[500px] absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 text-[1.5rem] text-white text-center leading-[1.5rem]"
-    >
-      Descubre el poder de Forex: donde cada transacción es una oportunidad y cada decisión
-      informada es un paso más cerca del éxito financiero
-    </h1>
   </section>
 </template>
