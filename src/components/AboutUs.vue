2aa4dc077a874ad8c1733ea7f3f9781b7e9ef8fe
Jimmy Sandoval <jsandovaldev.pe@gmail.com>
2024-01-30 21:12:20 -0500

    feat: Add responsive design to about us section

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,12 +1,18 @@
 <template>
   <section
-    class="w-full max-w-[76.5rem] mx-auto flex items-center justify-center gap-[6.25rem] pt-[26.875rem] pb-[5.3125rem] px-[1.5rem] sm:px-[2rem] md:px-[3rem] lg:px-[4rem]"
+    class="w-full max-w-[76.5rem] mx-auto flex flex-col md:flex-row items-center justify-center gap-[4rem] md:gap-[6.25rem] pt-[26.875rem] pb-[5.3125rem] px-[1.5rem] sm:px-[2rem] md:px-[3rem] lg:px-[4rem]"
   >
-    <div class="w-full max-w-[26.25rem] flex flex-col items-start gap-[0.75rem]">
-      <h1 class="text-[1.75rem] text-primary leading-[1.75rem]">
+    <div
+      class="w-full max-w-[33rem] md:max-w-[26.25rem] flex flex-col items-center md:items-start gap-[0.75rem]"
+    >
+      <h1
+        class="text-[1.5rem] text-primary text-center leading-[1.75rem] md:text-left md:text-[1.75rem]"
+      >
         Conviértete en maestro de Forex y Trading: cada lección, tu camino al éxito.
       </h1>
-      <p class="font-raleway font-medium text-[1rem] text-secondary leading-[1.5rem]">
+      <p
+        class="font-raleway font-medium text-[1rem] text-center text-secondary leading-[1.5rem] md:text-left"
+      >
         Ofrecemos herramientas innovadoras y conocimientos expertos en Trading y Forex. Únete a
         nosotros para transformar tus inversiones. ¡Descubre tu potencial ahora!"
       </p>
